647536de93b3125acb25eaa65fd27b454803ee5f
Gabriel Vieira <vieiragabriel@id.uff.br>
2025-03-26 14:28:30 -0300

    Adding auth-guard.

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -6,14 +6,15 @@ import { ContactComponent } from './components/contact/contact.component'
 import { ProductFormComponent } from './components/product-form/product-form.component'
 import { ProductListAdminComponent } from './components/product-list-admin/product-list-admin.component'
 import { LoginComponent } from './components/login/login.component'
+import { authGuard } from './guards/auth/auth.guard'
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'product-list', component: ProductListComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'product-form', component: ProductFormComponent },
-  { path: 'product-list-admin', component: ProductListAdminComponent },
+  { path: 'product-form', component: ProductFormComponent, canActivate: [authGuard] },
+  { path: 'product-list-admin', component: ProductListAdminComponent, canActivate: [authGuard] },
   { path: 'login', component: LoginComponent }
 ];
 
